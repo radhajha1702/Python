26b03b468413cec26db0c56378a44c8d3a27722c
Radha Jha <radhajha1702@gmail.com>
2024-09-13 01:44:44 +0530

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,50 @@
+print("hello, Welcome to my computer quiz")
+
+
+playing= input("Do you wanna play the game? ")
+if playing!="yes":
+    quit()
+
+print("Okay! Let's play :")    
+
+answer=input("What does CPU mean? ")
+if answer =="central processing unit":
+    print("Correct! ")
+
+else:
+    print("Sorry incorrect statement! ") 
+
+answer=input("What does Girraffe look like? ")
+if answer =="tall":
+    print("Correct! ")
+
+else:
+    print("Sorry incorrect statement! ")  
+
+answer=input("what is the capital of Uttar Pradesh? ")
+if answer =="Lucknow":
+    print("Correct! ")
+
+else:
+    print("Sorry incorrect statement! ")  
+
+answer=input("What are you studying ")
+if answer =="Python":
+    print("Correct! ")
+
+else:
+    print("Sorry incorrect statement! ")  
+
+answer=input("What does moon look like? ")
+if answer =="Perspective matters to answer this subjective question":
+    print("Correct! ")
+
+else:
+    print("Sorry incorrect statement! ")  
+
+answer=input("What day is today? ")
+if answer =="A wonderful day":
+    print("Correct! ")
+
+else:
+    print("Sorry incorrect statement! ")         
